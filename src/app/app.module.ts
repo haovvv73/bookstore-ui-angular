3dc746085a98066849e2cd9523e408e16e98c010
haovvv73 <haovvv73@gmail.com>
2023-09-27 00:30:04 +0700

    finish dashboard, login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  dataSource: Array<Book> = []
+  displayedColumn: Array<string> = ['id', 'title', 'author', 'category', 'price', 'action']
+
+  ngOnInit(): void {
+    this.dataSource = [
+      {
+        id: 1,
+        title: 'mua he khong ten',
+        author: 'nguyen nhat anh',
+        category: 'tieu thuyet',
+        price: 100.0
+      },
+      {
+        id:1,
+        title:'mua he khong ten',
+        author:'nguyen nhat anh',
+        category:'tieu thuyet',
+        price: 100.0
+      },
+      {
+        id:1,
+        title:'mua he khong ten',
+        author:'nguyen nhat anh',
+        category:'tieu thuyet',
+        price: 100.0
+      }
+    ]
+  }
 
 }
