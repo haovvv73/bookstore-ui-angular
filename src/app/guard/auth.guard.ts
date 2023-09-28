ef6f9c3b0fe990db6518ab08be5468f8174d7e5a
haovvv73 <haovvv73@gmail.com>
2023-09-29 00:38:02 +0700

    authguard - validate

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,5 +1,19 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import {inject} from '@angular/core';
+import { TokenServiceService } from '../service/tokenService.service';
+import { UserDaoService } from '../service/user-dao.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+
+  const path = route.url[0].path
+  // inject 
+  const router = inject(Router)
+  const tokenService = inject(TokenServiceService)
+  const userDao = inject(UserDaoService)
+
+  // check path 
+  if( !(path === 'dashboard') ) return false
+
+
   return true;
 };
